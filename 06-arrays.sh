36b9ec501b785092e648fae361d711e52b108154
MuraliKrishna7993 <muralimsdian1997@gmail.com>
2024-01-15 15:04:42 +0530

    vaiables examples

--- 06-arrays.sh
+++ 06-arrays.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
-
+NUMBER1=$1
+NUMBER2=$2
 FRUITS=("Apple" "Banana" "Mango")
 echo "${FRUITS[0]}"
 echo "${FRUITS[1]}"
 echo "${FRUITS[2]}"
 echo "${FRUITS[@]}"
+
+
+echo "sum of numbers: $((NUMBER1+NUMBER2))"
