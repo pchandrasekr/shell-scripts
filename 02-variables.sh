5bba3838c9b7063d027f773ed97d74eaa867eb9b
MuraliKrishna7993 <muralimsdian1997@gmail.com>
2024-01-15 14:23:41 +0530

    vaiables examples

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,7 +1,8 @@
-#!      /bin/bash
+#!/bin/bash
+PERSON1=Ramesh
+PERSON2=Suresh
 
-
-echo "$1: Hello $2, how are you?"
-echo "$2: Hello $1, I am doing good, how are you?"
-echo "$1: come to party $2 "
-echo "$2: ok $1, at what time?"
+echo "$PERSON1: Hello $PERSON2, how are you?"
+echo "$PERSON2: Hello $PERSON1, I am doing good, how are you?"
+echo "$PERSON1: come to party $PERSON2 "
+echo "$PERSON2: ok $PERSON1, we will meet."
